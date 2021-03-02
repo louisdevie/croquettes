f9a00a111246f41d9e6ac78bba527ad53e0b65f5
K039 <56866204+k39dev@users.noreply.github.com>
2021-03-02 17:22:00 +0100

    Add files via upload

---
+++ croquettes.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void RTCSetTime(int now) {}
+
+int RTCGetTime() {}
+
+int checkButtons() {}
+
+void displayTitle(String text) {}
+
+void displayContent(byte symbol, String text) {}
+
+void dispense(int amount) {}
